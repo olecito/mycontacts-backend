546a7f80e41ff86bef198533c459ac0d69f11d75
olecito <olezurita@icloud.com>
2023-07-30 18:39:58 +0200

    concatenamos llamados para ahorro de lineas

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getContact } = require('../controllers/contactController')
+const {getContacts, createContact, getContact, updateContact, deleteContact} = require('../controllers/contactController')
 
 
-router.route("/").get(getContact);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/").get();
-
-router.route("/").post();
-
-router.route("/:id").put();
-
-router.route("/:id").delete();
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
