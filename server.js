f02d0d6763897cdf8b6d7bf1ae46f2d315288f4d
olecito <olezurita@icloud.com>
2023-07-30 19:10:49 +0200

    SET routers

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use('/api/contacts', require('./routes/contactRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
